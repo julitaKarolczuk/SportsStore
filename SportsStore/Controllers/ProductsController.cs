1367255b26d3ebeea96d9f11d9b5d769969e992b
Krzysztof Kirylowicz <krzysztof.kirylowicz@mav.pl>
2018-01-13 13:58:34 +0100

    -

--- SportsStore/Controllers/ProductsController.cs
+++ SportsStore/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Models;
+using System.IO;
 
 namespace SportsStore.Controllers
 {
@@ -19,6 +20,16 @@ namespace SportsStore.Controllers
         {
             var products = db.Products.Include(p => p.Category);
             return View(products.ToList());
+        }
+
+        public ActionResult Bestsellers()
+        {
+            var products = db.Order_Product.GroupBy(po => po.Product)
+                .ToDictionary(gp => gp.Key, gp => gp.Sum(p => p.Amount))
+                .OrderByDescending(p=>p.Value)
+                .Take(10);
+
+            return View(products);
         }
 
         // GET: Products/Details/5
@@ -48,10 +59,18 @@ namespace SportsStore.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,CategoryId,Price,Picture,Hidden,Producer,Description,ShortDescription")] Product product)
+        public ActionResult Create([Bind(Include = "Id,Name,CategoryId,Price,Picture,Hidden,Producer,Description,ShortDescription")] Product product, HttpPostedFileBase file)
         {
             if (ModelState.IsValid)
             {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/Images"), _FileName);
+                    file.SaveAs(_path);
+                    product.Picture = $"/Images/{_FileName}";
+                }
+
                 db.Products.Add(product);
                 db.SaveChanges();
                 return RedirectToAction("Index");
